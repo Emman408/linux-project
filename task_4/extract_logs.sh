7ad07c8a11300b4fdec7faab6c15593200a56652
Emman408 <36513446+Emman408@users.noreply.github.com>
2025-04-11 10:32:06 -0300

    Update extract_logs.sh

--- task_4/extract_logs.sh
+++ task_4/extract_logs.sh
@@ -1,14 +1,13 @@
-sudo tee /usr/local/bin/extract_logs.sh > /dev/null <<'EOF'
 #!/usr/bin/env bash
+
 OUT=/var/log/critical_events
-mkdir -p \$OUT
+mkdir -p "$OUT"
 
 # Failed SSH logins
-grep "Failed password" /var/log/remote/<hostname>.log > \$OUT/failed_ssh.log
+grep "Failed password" /var/log/remote/student.log > "$OUT/failed_ssh.log"
 
 # Service failures (using journalctl)
-journalctl -p err -b | tee \$OUT/service_failures.log
+journalctl -p err -b > "$OUT/service_failures.log"
 
 # System reboots
-last reboot | head -n 10 > \$OUT/reboots.log
-EOF
+last reboot | head -n 10 > "$OUT/reboots.log"
